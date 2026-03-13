519d5f39f15ed1c386ebb2087ddd81cdafd83831
= <=>
2026-03-13 14:59:27 +0100

    fuck python on nixos

--- python3.nix
+++ python3.nix
@@ -1,17 +1,19 @@
 { config, pkgs, ... }:
 
+# Ss far as I know, I can't get this working and Im giving up
+
 {
-
-  # For UV usage
-  programs.nix-ld.enable = true;
-
-  # Python with jupyter notebook
   environment.systemPackages = with pkgs; [
     python313
-    python313Packages.jupyter-core
-    python313Packages.notebook
-    python313Packages.pip
-    python313Packages.uv
-
+    uv
   ];
+
+  programs.nix-ld = {
+    enable = true;
+    libraries = with pkgs; [
+      stdenv.cc.cc
+      zlib
+      openssl
+    ];
+  };
 }

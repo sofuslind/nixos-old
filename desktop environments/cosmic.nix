83f14f73f53059b02f9c3a97f64b33544fa09a13
Sofus Lind <43791049+sofuslind@users.noreply.github.com>
2026-03-24 21:14:57 +0100

    librewolf first install fix, vsix packages included for vscodium, and cosmic home manager testing started

--- desktop environments/cosmic.nix
+++ desktop environments/cosmic.nix
@@ -1,6 +1,22 @@
 { config, pkgs, ... }:
-
+let
+  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz";
+in
 {
+  imports = [
+    (import "${home-manager}/nixos")
+    ../dotfiles/alacritty.nix
+
+  ];
+
+  home-manager.users.sofushl = {
+    # The home.stateVersion option does not have a default and must be set
+    home.stateVersion = "18.09";
+    # Here goes the rest of your home-manager config, e.g. home.packages = [ pkgs.foo ];
+
+    #WIP
+  };
+
   # Cosmic greeter as displaymanager
   services.displayManager.cosmic-greeter.enable = true;
 
@@ -17,9 +33,5 @@
     cosmic-wallpapers
     flatpak
     pop-icon-theme
-  ];
-
-  imports = [
-    ./dotfiles/alacritty.nix
   ];
 }
--- dotfiles/librewolf/librewolf.nix
+++ dotfiles/librewolf/librewolf.nix
@@ -19,23 +19,6 @@ in
   programs.firefox = {
     enable = true;
     package = pkgs.librewolf;
-
-    # Force install the theme via enterprise policy
-    policies = {
-      ExtensionSettings = {
-        "minimal-dark-red@theme" = {
-          installation_mode = "force_installed";
-          install_url = "file://${myTheme}/theme.xpi";
-        };
-      };
-    };
-
-    # Activate the theme by default
-    preferences = {
-      "extensions.activeThemeID" = "minimal-dark-red@theme";
-      "xpinstall.signatures.required" = false;
-    };
-  };
-
-  environment.etc."librewolf/policies/policies.json".source = /etc/firefox/policies/policies.json;
+  }
+  environment.etc."librewolf/policies/policies.json".source = ./policies.json;
 }

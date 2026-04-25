de6abd2c2dc1e19191b440d88e67235e3c9828d4
Sofus Højberg Lind <43791049+sofuslind@users.noreply.github.com>
2026-04-25 10:22:56 +0200

    dark mode fixed?

--- niri/home.nix
+++ niri/home.nix
@@ -8,20 +8,36 @@
 {
   gtk = {
     enable = true;
-    colorScheme = "dark";
 
     theme.name = "Adwaita-dark";
 
+    gtk3 = {
+      extraConfig.gtk-application-prefer-dark-theme = 1;
+      theme.name = "Adwaita-dark";
+    };
     gtk4 = {
       colorScheme = "dark";
-      theme.name = "Adawaita-dark";
+      theme.name = "Adwaita-dark";
+    };
+  };
+
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
     };
   };
 
   xdg = {
+    enable = true;
+
     configFile."sunsetr/sunsetr.toml" = {
       source = ./config/sunsetr.toml;
       force = true;
+    };
+
+    portal = {
+      enable = true;
+      extraPortals = [ pkgs.xdg-desktop-portal-wlr ];
     };
   };
 }
